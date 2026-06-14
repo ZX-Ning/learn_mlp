f08c614e079f729e38b073b767a550e731e21bc8
ZX-Ning <code@zxning.com>
2026-06-14 12:12:17 +0800

    Optimize batch traning with matrices

--- mlp.cpp
+++ mlp.cpp
@@ -3,28 +3,41 @@
 
 #include <algorithm>
 #include <cstdio>
+#include <memory>
 #include <print>
-#include <span>
-//
-#include <Eigen/Dense>
+#include <random>
+#include <ranges>
 //
 #include <omp.h>
+//
+#include <Eigen/Dense>
 
 #include "data.h"
 
-Data train_data[60'000];
-Data test_data[10'000];
+using std::unique_ptr;
+constexpr int HIDDEN_LAYER_1_SIZE = 64;
+constexpr int EPOCHS = 30;
+constexpr float RATE = 5.f;
+constexpr int BATCH_SIZE = 100;
+constexpr int TRAIN_DATA_SIZE = 60'000;
+constexpr int TEST_DATA_SIZE = 10'000;
 
-void read_data() {
+struct Mnist {
+    Data train_data[TRAIN_DATA_SIZE];
+    Data test_data[TEST_DATA_SIZE];
+};
+
+unique_ptr<Mnist> read_data() {
+    auto result = std::make_unique<Mnist>();
     FILE* f_train_in = fopen("data/mnist/mnist_train.bin", "rb");
     FILE* f_test_in = fopen("data/mnist/mnist_test.bin", "rb");
-    fread(train_data, sizeof(Data), 60'000, f_train_in);
-    fread(test_data, sizeof(Data), 10'000, f_test_in);
+    fread(result->train_data, sizeof(Data), 60'000, f_train_in);
+    fread(result->test_data, sizeof(Data), 10'000, f_test_in);
     fclose(f_train_in);
     fclose(f_test_in);
+    return result;
 }
 
-constexpr int HIDDEN_LAYER_1_SIZE = 64;
 using Input = Eigen::Vector<float, 28 * 28>;
 using Hide1 = Eigen::Vector<float, HIDDEN_LAYER_1_SIZE>;
 using Output = Eigen::Vector<float, 10>;
@@ -71,26 +84,8 @@ struct Weights {
     }
 };
 
-#pragma omp declare reduction(weights_add:Weights : omp_out += omp_in) \
-    initializer(omp_priv = Weights{})
-
 float sigmoid(float x) {
     return 1.f / (1.f + std::exp(-x));
-}
-
-// float sigmoid_dx(float x) {
-//     return sigmoid(x) * (1 - sigmoid(x));
-// }
-
-Output delta(const Output& output, int label) {
-    Output expected{};
-    expected[label] = 1.f;
-    auto delta = output - expected;
-    return delta;
-}
-
-float loss(const Output& delta) {
-    return delta.squaredNorm() / 2.f;
 }
 
 struct Result {
@@ -131,6 +126,42 @@ Weights backward(const Output& delta, const Weights& weights, const Result& forw
     return gradient;
 }
 
+using InputBatch = Eigen::Matrix<float, 28 * 28, Eigen::Dynamic>;
+struct ResultBatch {
+    InputBatch input;
+    // Eigen::Matrix<float, HIDDEN_LAYER_1_SIZE, Eigen::Dynamic> z1;
+    Eigen::Matrix<float, HIDDEN_LAYER_1_SIZE, Eigen::Dynamic> a1;
+    // Eigen::Matrix<float, 10, Eigen::Dynamic> z2;
+    Eigen::Matrix<float, 10, Eigen::Dynamic> y_hat;
+};
+ResultBatch forward_batch(const InputBatch& in, const Weights& weights) {
+    auto z1 = (weights.w1 * in).colwise() + weights.b1;
+    auto a1 = (z1).unaryExpr(&sigmoid);
+    auto z2 = (weights.w2 * a1).colwise() + weights.b2;
+    auto y_hat = (z2).unaryExpr(&sigmoid);
+    return {in, std::move(a1), std::move(y_hat)};
+}
+
+Weights backward_batch(
+    const Eigen::Matrix<float, 10, Eigen::Dynamic>& delta,
+    const Weights& weights,
+    const ResultBatch& forward
+) {
+    int batch_size = delta.cols();
+    Weights gradient{};
+
+    auto d_z2 = delta.cwiseProduct(forward.y_hat.unaryExpr([](float x) { return x * (1 - x); }));
+    gradient.b2 = d_z2.rowwise().sum() / batch_size;
+    gradient.w2 = d_z2 * forward.a1.transpose() / batch_size;
+    // Keep batch dimension; only average when forming parameter gradients.
+    auto d_a1 = weights.w2.transpose() * d_z2;
+    auto d_z1 = d_a1.cwiseProduct(forward.a1.unaryExpr([](float x) { return x * (1 - x); }));
+    gradient.b1 = d_z1.rowwise().sum() / batch_size;
+    gradient.w1 = d_z1 * forward.input.transpose() / batch_size;
+
+    return gradient;
+}
+
 Weights randomWeights() {
     using w1_T = decltype(Weights::w1);
     using w2_T = decltype(Weights::w2);
@@ -142,69 +173,21 @@ Weights randomWeights() {
     };
 }
 
-Input inputFromRaw(uint8_t* data) {
-    return Eigen::Map<Eigen::Vector<uint8_t, 28 * 28>>(data, 28 * 28).cast<float>() / 255.f;
+Input inputFromRaw(const uint8_t* data) {
+    return (Eigen::Map<Eigen::Vector<uint8_t, 28 * 28>>(
+                const_cast<uint8_t*>(data), 28 * 28
+           )
+                .cast<float>()) /
+           255.f;
 }
 
-constexpr int EPOCHS = 100;
-constexpr float RATE = 0.5f;
-constexpr int BATCH_SIZE = 100;
-
-Weights train() {
-    constexpr size_t size = std::size(train_data);
-    Weights weights = randomWeights();
-
-    // training loop
-    for (int epoch = 0; epoch < EPOCHS; epoch++) {
-        float total_loss = 0.f;
-        for (int i = 0; i < size; i += BATCH_SIZE) {
-            Weights gradient{};
-            float batch_total_loss = 0.f;
-            int end = std::min<int>(i + BATCH_SIZE, size);
-            int batch_size = end - i;
-// one batch
-#pragma omp parallel for reduction(+ : batch_total_loss) \
-    reduction(weights_add : gradient)
-            for (int j = i; j < end; j++) {
-                auto& row = train_data[j];
-                auto input = inputFromRaw(row.data);
-                auto forward_result = forward(input, weights);
-                auto d = delta(forward_result.y_hat, row.label);
-                auto l = loss(d);
-                auto gradient_step = backward(d, weights, forward_result);
-                batch_total_loss += l;
-                gradient += gradient_step;
-            }
-
-            gradient /= batch_size;
-            total_loss += batch_total_loss;
-            gradient *= RATE;
-            weights -= gradient;
-        }
-        std::println("Epoch: {}, Avg loss: {}", epoch + 1, total_loss / size);
-    }
-    return weights;
-}
-
-int num_pick(const std::span<float, 10> guess) {
-    float max = -INFINITY;
-    int pick = 0;
-    for (int i = 0; i < 10; i++) {
-        if (guess[i] > max) {
-            max = guess[i];
-            pick = i;
-        }
-    }
-    return pick;
-}
-
-void test(const Weights& weights) {
+void test(const Weights& weights, const Mnist& data) {
     int errors = 0;
-    for (size_t i = 0; i < std::size(test_data); i++) {
-        auto& row = test_data[i];
+    for (size_t i = 0; i < TEST_DATA_SIZE; i++) {
+        auto& row = data.test_data[i];
         auto forward_result = forward(inputFromRaw(row.data), weights);
-        auto guess = forward_result.y_hat.array();
-        int pick = num_pick(std::span<float, 10>{guess.begin(), guess.end()});
+        Eigen::Index pick = 0;
+        forward_result.y_hat.maxCoeff(&pick);
         if (pick != row.label) {
             // std::println("Not match on test {}, expect {}, get {}.", i, row.label, pick);
             errors++;
@@ -212,18 +195,63 @@ void test(const Weights& weights) {
     }
     std::println(
         "Test done. {} tested. Errors: {}. Error rate: {}",
-        std::size(test_data),
+        TEST_DATA_SIZE,
         errors,
-        static_cast<float>(errors) / std::size(test_data)
+        static_cast<float>(errors) / TEST_DATA_SIZE
     );
+}
+
+Weights train(const Mnist& data) {
+    Weights weights = randomWeights();
+    InputBatch batch_buf;
+    Eigen::Matrix<float, 10, Eigen::Dynamic> expected_buf;
+
+    std::vector<int> indices =
+        std::ranges::views::iota(0, TRAIN_DATA_SIZE) |
+        std::ranges::to<std::vector<int>>();
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    // Shuffle the indices randomly
+
+    // training loop
+    for (int epoch = 0; epoch < EPOCHS; epoch++) {
+        std::ranges::shuffle(indices, g);
+        float total_loss = 0.f;
+        for (int i = 0; i < TRAIN_DATA_SIZE; i += BATCH_SIZE) {
+            int end = std::min<int>(i + BATCH_SIZE, TRAIN_DATA_SIZE);
+            int batch_size = end - i;
+            // one batch
+            batch_buf.resize(Eigen::NoChange, batch_size);
+            expected_buf.resize(Eigen::NoChange, batch_size);
+            expected_buf.setZero();
+            for (int j = i, k = 0; j < end; j++, k++) {
+                auto& row = data.train_data[indices[j]];
+                Input input = inputFromRaw(row.data);
+                batch_buf.col(k) = input;
+                expected_buf(row.label, k) = 1.f;
+            }
+            auto forward_result = forward_batch(batch_buf, weights);
+            Eigen::Matrix<float, 10, Eigen::Dynamic> d = forward_result.y_hat - expected_buf;
+            float l = d.squaredNorm() / 2.f;
+            Weights avg_gradient = backward_batch(d, weights, forward_result);
+            avg_gradient *= RATE;
+            weights -= avg_gradient;
+            total_loss += l;
+        }
+        std::println("Epoch: {}, Avg loss: {}", epoch + 1, total_loss / TRAIN_DATA_SIZE);
+        // test(weights, data);
+    }
+    return weights;
 }
 
 int main() {
     read_data();
-    auto weights = train();
+    auto data = read_data();
+    auto weights = train(*data);
     FILE* fOut = fopen("weights.bin", "wb");
     weights.serialize(fOut);
-    test(weights);
+    test(weights, *data);
     fclose(fOut);
     return 0;
 }
